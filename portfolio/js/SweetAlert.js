5849ae3da6387b55a5f17124075214449feb8477
Gaurav Maharjan <rijanmaharjan999@gmail.com>
2023-01-01 19:47:18 +0545

    Portfolio

---
+++ portfolio/js/SweetAlert.js
@@ -0,0 +1,49 @@
+// JavaScript source code
+
+
+/*$(document).ready(function () {
+    $('#dowload').click(
+        function () {
+            swal({
+                title: "Are you sure?",
+                text: "Do you want to download my CV?",
+                icon: "success",
+                buttons: true,
+                dangerMode: false,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        swal("File has been downloaded", {
+                            icon: "success",
+                        });
+                    }
+                });
+
+        })
+
+
+});*/
+
+
+
+
+//function download() {
+//    debugger;
+//    swal({
+//        title: "Are you sure?",
+//        text: "Do you want to download my CV?",
+//        icon: "success",
+//        buttons: true,
+//        dangerMode: false,
+//    })
+//        .then((willDelete) => {
+//            if (willDelete) {
+//                swal("File has been downloaded", {
+//                    icon: "success",
+//                });
+//            }
+//        });
+
+//};
+
+
